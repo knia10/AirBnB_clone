202209fa4da934d9df438a45774d28f0b5f119ec
LauSCaicedo <3383@holbertonschool.com>
2021-11-13 23:12:03 -0500

    Update Tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
 """
-Instantiating BaseModel
+Instantiating BaseModel.
 """
-import models
-from models.base_model import BaseModel
 import unittest
+from models.base_model import BaseModel
+import pycodestyle
 
 
 class TestBaseModel(unittest.TestCase):
     """
-    Starting Test
+    Starting Test.
     """
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Init testing enviroment
+        """
+        ...
+
+    def test_split(self):
+        str_g = 'Cristian Granada'
+        self.assertEqual(str_g.split(), ['Cristian', 'Granada'])
 
     def test_is_an_instance(self):
         """
@@ -18,6 +29,49 @@ class TestBaseModel(unittest.TestCase):
         """
         my_BaseModel = BaseModel()
         self.assertIsInstance(my_BaseModel, BaseModel)
+
+    def test_attributes(self):
+        """
+        Check if the class had corrects attributes
+        """
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "__str__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+    def test_id(self):
+        """
+        Check if the class had unique id's
+        """
+        my_BaseModel = BaseModel().id
+        my_1BaseModel = BaseModel().id
+        self.assertNotEqual(my_BaseModel, my_1BaseModel)
+
+    def test_save(self):
+        """
+        Check if the class save the update the date
+        """
+        my_BaseModel = BaseModel()
+        my_1BaseModel = my_BaseModel.updated_at
+        my_BaseModel.save()
+        my_2BaseModel = my_BaseModel.updated_at
+        self.assertNotEqual(my_1BaseModel, my_2BaseModel)
+
+    def test_style(self):
+        """
+        Check if the file had correct style
+        """
+        pstyle = pycodestyle.StyleGuide(quiet=True)
+        result = pstyle.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Clear testing enviroment
+        """
+        ...
 
 
 if __name__ == '__main__':
