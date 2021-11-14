10fce71eeb53130a72cdb802a13aa114ed6e107d
knia10 <kelly.stephania@hotmail.com>
2021-11-14 00:18:16 -0500

    Add test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,24 +1,54 @@
 #!/usr/bin/python3
 """
-Instantiating State
+Unittest for states
 """
 import models
+from models import state
 from models.base_model import BaseModel
 from models.state import State
 import unittest
+import os
+import pycodestyle
 
 
 class TestState(unittest.TestCase):
     """
     Starting Test
     """
+    @classmethod
+    def setUpClass(cls):
+        '''Test'''
+        print('\n****************** Init Testing ******************\n')
+        cls.my_state = State()
 
     def test_is_an_instance(self):
         """
-        Instantiating States
+        Instantiating states
         """
-        my_State = State()
-        self.assertIsInstance(my_State, State)
+        my_state = State()
+        self.assertIsInstance(my_state, State)
+
+    def test_type_instance(self):
+        """
+        type instance
+        """
+        self.assertIs(type(self.my_state.name), str)
+
+    def test_attributes(self):
+        self.assertTrue(hasattr(state, "__init__"))
+        self.assertTrue(hasattr(state, "__str__"))
+        self.assertTrue(state, "name")
+
+    def test_pycodestyle(self):
+        """Test style PEP8."""
+        pep8style = pycodestyle.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/state.py'])
+        self.assertEqual(result.total_errors, 0, "code style errors")
+
+    @classmethod
+    def tearDownClass(cls):
+        '''Test'''
+        print('\n****************** Finish Testing ******************\n')
 
 
 if __name__ == '__main__':
